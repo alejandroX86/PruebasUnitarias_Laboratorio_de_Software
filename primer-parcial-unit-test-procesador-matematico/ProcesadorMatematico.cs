e860f35c0b3a685506b5b28e1ccf6b423be847bc
Omar Alejandro Bazar <69990247+alejandroX86@users.noreply.github.com>
2024-05-07 21:02:52 -0300

    Add files via upload

---
+++ primer-parcial-unit-test-procesador-matematico/ProcesadorMatematico.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_parcial_unit_test_procesador_matematico
+{
+    public class ProcesadorMatematico
+    {
+        public int _resultado;
+
+        public void Sumar(int primerNumero, int segundoNumero)
+        {
+            this._resultado = primerNumero + segundoNumero;
+            if (this.EsNegativo())
+            {
+                this._resultado = 0;
+            }
+        }
+
+        public void Dividir(int dividendo, int divisor)
+        {
+            this._resultado = dividendo / divisor;
+            if (this.EsNegativo())
+            {
+                this._resultado = 0;
+            }
+        }
+        public bool EsNegativo()
+        {
+            return (this._resultado < 0);
+        }
+
+        public void CalcularMayor(int primerNumero, int segundoNumero)
+        {
+            if (primerNumero > segundoNumero)
+            {
+                this._resultado = primerNumero;
+            }
+            else if (primerNumero < segundoNumero)
+            {
+                this._resultado = segundoNumero;
+            }
+            else
+            {
+                this._resultado = segundoNumero;
+            }
+        }
+
+        public string GetResultado()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ primer-parcial-unit-test-procesador-matematico/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+// hago el main para los calculos
+
+namespace primer_parcial_unit_test_procesador_matematico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            ProcesadorMatematico procesador = new ProcesadorMatematico();
+
+            
+            procesador.Sumar(5, 3);
+            Console.WriteLine("Resultado de la suma: " + procesador.GetResultado());
+
+            
+            procesador.Dividir(10, 2);
+            Console.WriteLine("Resultado de la división: " + procesador.GetResultado());
+
+            
+            procesador.CalcularMayor(7, 12);
+            Console.WriteLine("Resultado del cálculo del mayor: " + procesador.GetResultado());
+
+            
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
